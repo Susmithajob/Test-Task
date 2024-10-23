9676b24e1a707ea7c2dbaa26bd380b793facaf6a
Susmitha <susmithajob93@gmail.com>
2024-10-23 11:30:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+const port = 3000;
+const fs = require('fs');
+
+
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('home')
+  })
+
+  app.get('/details', (req, res) => {
+    const studentName = req.query.studentName;
+    if (!studentName) {
+        res.render('error',{status:400,message:`Student name required`});
+    }
+    fs.readFile('./student.json','utf-8',(err,data)=>
+    {
+        if (err) {
+            res.render('error',{status:500,message:`Failed to load the file`});
+        }
+        const details = JSON.parse(data);
+        const student = details.find(s => s.name.toLowerCase() === studentName.toLowerCase());
+        if (student) {
+            res.render('details',
+            {name:student.name,
+             age:student.age,
+             grade:student.grade,
+             totalMark:student.total_marks
+            });      
+        }
+         else {
+            res.render('error',{status:404,message:`Student named ${studentName} Not found`});
+        }
+    }) 
+  })
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
